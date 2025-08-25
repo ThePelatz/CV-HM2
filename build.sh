865d9ae307a865d99e60e87d89409123dc2e10a9
ThePelatz <zorenmartinez5@gmail.com>
2025-08-25 12:31:56 +0200

    added trained model

--- build.sh
+++ build.sh
@@ -12,11 +12,6 @@ if [ ! -f "include/json.hpp" ]; then
     wget https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp -O include/json.hpp
 fi
 
-# Download our trained model if not already present
-if [ ! -f "simple_card_classifier_traced.pt" ]; then
-    wget "https://github.com/davidebaggio/CV-HW2/releases/download/v1/simple_card_classifier_traced.pt" -O simple_card_classifier_traced.pt
-fi
-
 # Build the project
 cmake -B build
 make -j6 -C build
